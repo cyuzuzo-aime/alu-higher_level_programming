96ad21b87093dbd421f629041cfb5ed7df58f5bc
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-03-05 04:02:46 -0800

    fix

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for i in range(25, -1, -1):
-    print("{:c}".format(i + 65) if i % 2 == 0 else "{:c}".format(i + 97), end="")
+    if i % 2 == 0:
+        print("{:c}".format(i + 65), end="")
+    else:
+        print("{:c}".format(i + 97), end="")
