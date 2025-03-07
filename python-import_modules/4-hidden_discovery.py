450f35b23c764e77e1b25b3d6c51599aafb86ebb
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-03-07 03:30:28 -0800

    fix

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -4,11 +4,10 @@ import hidden_4
 
 def main():
     names = dir(hidden_4)
-    filtered_names = sorted(name for name in names if not name.startswith('__'))
-    for name in filtered_names:
+    filterednames = sorted(name for name in names if not name.startswith('__'))
+    for name in filterednames:
         print(name)
 
 
 if __name__ == "__main__":
     main()
-
