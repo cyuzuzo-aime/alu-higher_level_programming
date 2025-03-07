049c65c3b46bbe731cd55f8c6d9bee5d772bdd01
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-03-07 08:46:04 -0800

    new file: 101-nqueens.py

---
+++ python-more_classes/101-nqueens.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+import sys
+
+def is_safe(board, row, col, N):
+    for i in range(row):
+        if board[i] == col or abs(board[i] - col) == abs(i - row):
+            return False
+    return True
+
+def solve_nqueens(N, row=0, board=[]):
+    if row == N:
+        print([[i, board[i]] for i in range(N)])
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            solve_nqueens(N, row + 1, board + [col])
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    solve_nqueens(N)
+
+if __name__ == "__main__":
+    main()
+
