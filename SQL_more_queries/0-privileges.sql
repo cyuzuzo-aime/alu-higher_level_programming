c4944f9f129061d136fec597d9d2cb4e613c3260
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-03-13 16:23:45 -0700

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,9 +1,5 @@
 -- SQL
 
--- List privileges for user_0d_1
-SELECT 'Grants for user_0d_1@localhost' AS '';
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- List privileges for user_0d_2
-SELECT 'Grants for user_0d_2@localhost' AS '';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
