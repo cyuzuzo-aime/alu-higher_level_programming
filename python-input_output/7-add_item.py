989b2ac2b63cc10803b4ec306e6a89de49e52e9d
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-03-13 05:03:31 -0700

    fix

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 
-"""Module for adding command-line arguments to a list and saving them to a JSON file.
-
-This script loads a list from a JSON file named 'add_item.json', adds all
-command-line arguments to the list, and saves the updated list back to the file.
-If the file doesn't exist, it creates the file and initializes the list.
-"""
+"""Module for adding command-line arguments to a list"""
 
 import sys
 from save_to_json_file import save_to_json_file
