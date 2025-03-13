648855eabe9abb985b82f989863e389c7904ee83
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-03-13 16:03:29 -0700

    11-genre_id_all_shows.sql

--- SQL_more_queries/11-genre_id_all_shows.sql
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
--- List tv shows and their genre ids, sorted by tv show title and genre id
+-- SQL
+
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
