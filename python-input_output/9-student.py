936f2c4629fcefe64898c431e2c8be6e59f2d071
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-03-13 07:51:12 -0700

    9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,19 +4,15 @@
 
 
 class Student:
-    
     """Class representing a student with first name, last name, and age."""
-    
 
     def __init__(self, first_name, last_name, age):
         """Initializes the student with first name, last name, and age."""
-        
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-
     def to_json(self):
         """Returns the dictionary representation of the Student instance."""
-        
         return self.__dict__
+
