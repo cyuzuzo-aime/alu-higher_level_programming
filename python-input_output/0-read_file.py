01991199a073b6e55a45f7484a72a2ff8ae727c5
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-03-13 04:10:02 -0700

    fix

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 
 """Module that defines the read_file function."""
 
+
 def read_file(filename=""):
     """Read a UTF-8 encoded file and print its contents."""
     with open(filename, encoding="utf-8") as f:
