6330a21ca80f3e2bee28d55bb59cee13a623fb21
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-03-13 08:06:51 -0700

    10-student.py

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -13,12 +13,7 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns the dictionary representation of the Student instance.
-
-        If attrs is a list of strings, only attributes matching names in attrs are retrieved.
-        Otherwise, all attributes are retrieved.
-        """
+        """Returns the dictionary representation of the Student instance."""
         if isinstance(attrs, list) and all(isinstance(attr, str) for attr in attrs):
             return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
         return self.__dict__
-
