d02e811b4b5049b266cba99d78ed406186929dc1
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-03-13 15:44:45 -0700

    9-cities_by_state_join.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
--- List tv shows and their genre ids, sorted by tv show title and genre id
+-- List shows with at least one genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
