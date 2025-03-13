0315d35a6925ad1392513f8243bc17a3f6f1602b
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-03-13 04:40:54 -0700

    fix

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""Module for adding command-line arguments to a list and saving it to a file."""
+
+import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+
+def main():
+    """Adds arguments to a list and saves them to a JSON file."""
+    
+    try:
+        items = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        items = []
+    
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, 'add_item.json')
+
+
+if __name__ == '__main__':
+    main()
