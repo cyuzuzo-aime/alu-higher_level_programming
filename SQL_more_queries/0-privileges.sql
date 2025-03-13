3bb2c6f10faffbba26d3945ae457b91f69c8f319
cyuzuzo-aime <a.cyuzuzo2@alustudent.com>
2025-03-13 16:18:20 -0700

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,6 +1,4 @@
 -- SQL
 
-SELECT user, host, privilege_type
-FROM information_schema.user_privileges
-WHERE user IN ('user_0d_1', 'user_0d_2') AND host = 'localhost'
-ORDER BY user, privilege_type;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
