20cc440299a2824c01dac2c97744af1fa3c03c89
cyuzuzo-aime <a.cyuzuzo@alustudent.com>
2025-05-31 14:04:20 -0700

    new file fix

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+function add (a, b) {
+  return a + b;
+}
+
+const firstInt = parseInt(process.argv[2]);
+const secondInt = parseInt(process.argv[3]);
+
+console.log(add(firstInt, secondInt));
